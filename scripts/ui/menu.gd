a0e911d9bedda6f42466cf3eb284205a3a478e0e
Animatronicat <luckybarsik2710@gmail.com>
2026-02-02 20:12:47 +0300

    +menu&sett

---
+++ scripts/ui/menu.gd
@@ -0,0 +1,24 @@
+extends Control
+
+@onready var settings: Control = $Settings
+@onready var panel: Panel = $Panel
+
+func _ready() -> void:
+	pass
+
+
+func _on_play_btn_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level.tscn")
+
+func _on_stngs_btn_pressed() -> void:
+	settings.show()
+	panel.hide()
+
+
+func _on_exit_btn_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_back_btn_pressed() -> void:
+	settings.hide()
+	panel.show()
